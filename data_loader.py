415c36207226afc25de675cdc12ce3ae1803f3f0
Alexei11111 <grebennikovaleksej751@gmail.com>
2025-09-22 11:56:03 +0300

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,11 @@
+import gdown
+import pandas as pd
+
+FILE_ID = "12MkiB6pcc9Tie_qV3v1xFJuJvQY83qDl"
+URL = f"https://drive.google.com/uc?id={FILE_ID}"
+
+OUTPUT = "dataset.csv"  # Тут качаем файл
+gdown.download(URL, OUTPUT, quiet=False)
+
+raw_data = pd.read_csv(OUTPUT)
+print(raw_data.head(10)) # Тут выводим первые 10 значений из датасета
