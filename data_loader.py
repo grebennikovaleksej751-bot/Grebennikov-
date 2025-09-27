c9e7b8fb55ef8b3da6800ceced9e1fa023ba3071
Alexei11111 <grebennikovaleksej751@gmail.com>
2025-09-28 00:31:24 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -7,5 +7,6 @@ URL = f"https://drive.google.com/uc?id={FILE_ID}"
 OUTPUT = "dataset.csv"  # Тут качаем файл
 gdown.download(URL, OUTPUT, quiet=False)
 
-raw_data = pd.read_csv(OUTPUT)
+raw_data = pd.read_csv(OUTPUT,sep=';', encoding='utf-8-sig')
+
 print(raw_data.head(10)) # Тут выводим первые 10 значений из датасета
